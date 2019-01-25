b233fcc5f818b56b249e68a6d88a2abd4850755a
BrianArchibald <brianallenarchibald@gmail.com>
2019-01-24 20:52:06 -0600

    gmaps in, responsive done almost

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,35 @@ const wineMenu = document.getElementById("wine-menu-listing");
 const cocktailMenu = document.getElementById("cocktails-menu-listing");
 const pizzaMenu = document.getElementById("pizza-menu-listing");
 
-// const beerItems = document.querySelector(".beer");
-// const wineItems = document.querySelector(".wine");
-// const cocktailItems = document.querySelector(".cocktails");
-//const pizzaItems = document.querySelector(".pizza");
 const beerItems = Array.from(document.querySelectorAll(".beer"));
 const wineItems = Array.from(document.querySelectorAll(".wine"));
 const cocktailItems = Array.from(document.querySelectorAll(".cocktails"));
 const pizzaItems = Array.from(document.querySelectorAll(".pizza"));
+
+const menuIcon = document.getElementById("menu-icon");
+const crossIcon = document.getElementById("cross-icon");
+const mobileNav = document.getElementById("nav-bar-mobile");
+
+menuIcon.addEventListener("click", function() {
+	menuIcon.classList.add("remove");
+	menuIcon.classList.remove("display");
+	crossIcon.classList.add("display");
+	crossIcon.classList.remove("remove");
+
+	mobileNav.classList.add("show");
+	mobileNav.classList.remove("hide");
+});
+
+crossIcon.addEventListener("click", function() {
+	menuIcon.classList.remove("remove");
+	menuIcon.classList.add("display");
+	crossIcon.classList.remove("display");
+	crossIcon.classList.add("remove");
+
+	mobileNav.classList.remove("show");
+	mobileNav.classList.add("hide");
+});
+
 
 
 beerMenu.addEventListener("click", function(){
@@ -95,8 +116,14 @@ pizzaMenu.addEventListener("click", function(){
 
 });
 
-
-// let allElements = Array.from(document.querySelectorAll('.widget.hover'))
-// for (let element of allElements) {
-//   element.classList.remove('hover')
-// }
+function initMap(){
+	var location = {lat: 45.521901, lng: -122.690613};
+	var map = new google.maps.Map(document.getElementById("map"),{
+		zoom: 8,
+		center: location,
+	});
+	var marker = new google.maps.Marker({
+		position: location,
+		map: map
+	});
+};
